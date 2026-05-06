329564595f4b478096a1ae0b29d2f55292e31c15
tagawa-tomoaki <a24z5tt@aiit.ac.jp>
2026-05-06 13:03:50 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  root "hello#index"
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
+
+  get  'foo/bar'
+  get  'foo/baz'
 end
